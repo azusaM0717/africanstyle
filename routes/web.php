814a8730f8116ed3e6c8103e5912cda3023470eb
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2025-02-25 04:48:06 +0000

    make product controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//商品一覧ページのルート
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
