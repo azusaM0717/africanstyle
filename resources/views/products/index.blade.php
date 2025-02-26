d40a633987a9b28e02aa51035cfc0579fa04a619
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2025-02-26 14:59:46 +0000

    title

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,8 @@
     <title>商品一覧</title>
 </head>
 <body>
-  <h1>商品一覧</h1>
+  <h1>African Style</h1>
+  <h2>商品一覧</h2>
   <ul>
     @foreach ($products as $product)
       <li>{{ $product->name }} - {{ number_format($product->price) }}円</li>
